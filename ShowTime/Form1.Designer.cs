c8bff15b1b4d74b5dfc8ee3f3848d2a00b87e477
admin <admin@DESKTOP-PNVIAD5>
2024-01-12 13:37:39 +0200

    Add project files.

---
+++ ShowTime/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace ShowTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            labelTime = new Label();
+            SuspendLayout();
+            // 
+            // labelTime
+            // 
+            labelTime.AutoSize = true;
+            labelTime.BorderStyle = BorderStyle.Fixed3D;
+            labelTime.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            labelTime.Location = new Point(80, 50);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(142, 67);
+            labelTime.TabIndex = 0;
+            labelTime.Text = "Time";
+            labelTime.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(649, 161);
+            Controls.Add(labelTime);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label labelTime;
+    }
+}
---
+++ ShowTime/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ShowTime
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.Timer vTimer = new System.Windows.Forms.Timer();
+
+        private void ShowTime(object sender, EventArgs e)
+        {
+            // Отримуємо поточний час і дату
+            DateTime currentTime = DateTime.Now;
+
+            // Перетворення в рядок та відображення часу та дати
+            labelTime.Text = currentTime.ToString("HH:mm:ss  dd/MM/yyyy");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Встановлення тексту зараз, під час ініціалізації форми
+            labelTime.Text = DateTime.Now.ToString("HH:mm:ss  dd/MM/yyyy");
+
+            // Закріплення обробника
+            vTimer.Tick += new EventHandler(ShowTime);
+
+            // Встановлення інтервалу часу (в мілісекундах)
+            vTimer.Interval = 1000;
+
+            // Запуск таймера
+            vTimer.Start();
+        }
+    }
+}
